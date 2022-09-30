855f18ea2612048fc28072db89bab77af7da1186
Sergio Wilson <sergio.wilson-galvez@orion.com>
2022-09-30 11:38:21 -0500

    introduce story book

---
+++ stories/check.ts
@@ -0,0 +1,6 @@
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from './modules/material/material.module';
+
+import { MatXtnLoadingButtonModule } from '@matxtnd'
+import { LoadingButtonComponent } from './examples/loading-button/loading-button.component';
+

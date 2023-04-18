1053b270471f2b60dc15cb4a559c227ed4895883
Sergio Wilson <sergio.wilson@foxpoint.com>
2023-04-17 19:58:13 -0500

    refactor(upgrade to angular 14): angular 14 upgrade

--- projects/mat-xtnd/src/lib/utils/utils.module.ts
+++ projects/mat-xtnd/src/lib/utils/utils.module.ts
@@ -5,24 +5,21 @@ import { MatXtndThemeHelperComponent } from './components';
 import { MatXtndCssInjectorService, MatXtndScriptInjectorService, MatXtndThemeColorFinderService } from './services';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    MatButtonModule,
-  ],
-  declarations: [
-    MatXtndThemeHelperComponent
-  ],
-  exports: [
-    MatXtndThemeHelperComponent
-  ],
-  providers: [
-    MatXtndThemeColorFinderService,
-    MatXtndCssInjectorService,
-    MatXtndScriptInjectorService
-  ],
-  entryComponents: [
-    MatXtndThemeHelperComponent
-  ]
+    imports: [
+        CommonModule,
+        MatButtonModule,
+    ],
+    declarations: [
+        MatXtndThemeHelperComponent
+    ],
+    exports: [
+        MatXtndThemeHelperComponent
+    ],
+    providers: [
+        MatXtndThemeColorFinderService,
+        MatXtndCssInjectorService,
+        MatXtndScriptInjectorService
+    ]
 })
 export class MatXtnUtilsModule {
   constructor(colorFinder: MatXtndThemeColorFinderService) {

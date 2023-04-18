fd8cb729a543f17028de97e426cc100257a5122e
Sergio Wilson <sergio.wilson@foxpoint.com>
2023-04-17 19:46:19 -0500

    docs(upgrade to new version of storybook): storybook version 7.0

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,11 @@
 module.exports = {
   "stories": ["../stories/**/*.stories.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
-  "framework": "@storybook/angular",
-  core: {
-    builder: "webpack5"
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-mdx-gfm"],
+  "framework": {
+    name: "@storybook/angular",
+    options: {}
+  },
+  docs: {
+    autodocs: true
   }
 };

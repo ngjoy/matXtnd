5b7b0b0ada034381c584886661b973629748a246
Sergio Wilson <sergio.wilson@foxpoint.com>
2023-04-24 19:40:06 -0500

    refactor(update to angular 14): update versions of angular

--- projects/integration/src/app/app.component.ts
+++ projects/integration/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatXtndConfirmationDialogService } from '../../../mat-xtnd/src/lib/dialogs/services/confirmation-dialog.service';
 import { MatXtndSnackbarSuccessService } from '../../../mat-xtnd/src/lib/snackbar-success';
 
 @Component({
@@ -8,9 +9,17 @@ import { MatXtndSnackbarSuccessService } from '../../../mat-xtnd/src/lib/snackba
 })
 export class AppComponent {
   title = 'integration';
-  constructor(private readonly snackbarSuccessStatus: MatXtndSnackbarSuccessService) { }
+  constructor(
+    private readonly snackbarSuccessStatus: MatXtndSnackbarSuccessService,
+    private readonly confirmationDialog: MatXtndConfirmationDialogService,
+  ) { }
 
   successSnackbar() {
     this.snackbarSuccessStatus.Open('Activity Completed');
+  }
+
+  confirm() {
+    this.confirmationDialog.OnOkOrCancel({ title: 'Confirm', message: 'Are you sure you want to do this?' })
+      .subscribe(response => console.log(response));      ;
   }
 }
--- projects/mat-xtnd/src/lib/dialogs/components/confirmation-dialog/confirmation-dialog.component.ts
+++ projects/mat-xtnd/src/lib/dialogs/components/confirmation-dialog/confirmation-dialog.component.ts
@@ -4,9 +4,15 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 export type MatXtndConfirmationDialogActionType = "OK" | "CANCEL";
 export const MatXtndConfirmationDialogActionTypes: { [key in MatXtndConfirmationDialogActionType]: MatXtndConfirmationDialogActionType } = { OK: "OK", CANCEL: "CANCEL"}
 export const MatXtndConfirmationDialogButtonLabelsDefaults: { [key in MatXtndConfirmationDialogActionType]: string } = { OK: "Ok", CANCEL: "Cancel" }
-export type MatXtndConfirmationDialogButtonLabelsType = { OK: string, CANCEL: string}
+export type MatXtndConfirmationDialogButtonLabelsType = { OK: string, CANCEL: string }
+
 export type MatXtndConfirmationDialogParamsType = { title?: string, message: string, buttonLabels?: MatXtndConfirmationDialogButtonLabelsType }
 export type MatXtndConfirmationDialogParamsDataType = { title?: string, message: string, buttonLabels: MatXtndConfirmationDialogButtonLabelsType }
+
+export const MatXtndConfirmationDialogOKButtonLabelsDefaults = { OK: "Ok" }
+export type MatXtndConfirmationDialogOkButtonLabelsType = { OK: string }
+export type MatXtndConfirmationDialogOkParamsType = { title?: string, message: string, buttonLabels?: MatXtndConfirmationDialogOkButtonLabelsType }
+
 
 /** @dynamic */
 @Component({
